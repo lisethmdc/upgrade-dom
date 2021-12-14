c76b1d3e4a224f4cbb12164c8c3f2b360e17223e
lisethmdc <lismc1992@gmail.com>
2021-12-14 22:54:55 +0100

    Iteration#1

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+
+    //1.1
+    console.log(document.querySelector(".showme"))
+    //1.2
+    console.log(document.querySelector("#pillado"))
+    //1.3
+    console.log(document.querySelectorAll("p"))
+    //1.4
+    console.log(document.querySelectorAll(".pokemon"))
+    //1.5
+    console.log(document.querySelectorAll("[data-function='testMe']"))
+    //1.6
+    let showThird = document.querySelectorAll("[data-function='testMe']")
+
+    for (var i = 0; i < showThird; i++) {
+    }
+    console.log(showThird[2])
+
+}
